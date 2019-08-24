252e0325e73ce4eff2fe1c809976f35a538ecc59
Leandro Aguiar <le.aguiar@gmail.com>
2019-08-24 01:23:38 +0000

    iuh

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
-function chamarAPI(){
-	console.log("Chamando API");
-	const endpoint = "api/latest";
-	var oParametros = {
-		base: $("#moeda_origem").val()
-	};
-	$.get(endpoint, oParametros, callback );
-}
+const ovly = { };
 
-function callback(resultado){
-	// console.log(resultado);
-	
-	var sMoedaDestino = $("#moeda_destino").val();
-	var fValor = resultado.rates[sMoedaDestino];
-	
-	$("#valor_destino").text(fValor);
-	$("#moeda_destino_celula").text(sMoedaDestino);
+ovly.conversor = { 
+	versao: "1",
+	chamarAPI: function(){
+		console.log("Chamando API");
+		const endpoint = "api/latest";
+		var oParametros = {
+			base: $("#moeda_origem").val()
+		};
+		$.get(endpoint, oParametros, this.callback );
+	},
+	callback: function(resultado){
+		// console.log(resultado);
+		var sMoedaDestino = $("#moeda_destino").val();
+		var fValor = resultado.rates[sMoedaDestino];
+		$("#valor_destino").text(fValor);
+		$("#moeda_destino_celula").text(sMoedaDestino);
+	}
 }
